d0f4906a1dd59c57dc76b69ebe1901fdeb496b6a
chzonr <31703116+grav-acc@users.noreply.github.com>
2025-11-26 03:55:35 +0000

    update robots.txt

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,16 +1,8 @@
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = ({ site }) => {
-	const text = `
-User-agent: Googlebot
-User-agent: Bingbot
-User-agent: DuckDuckBot
-User-agent: archive.org_bot
+	const text = `User-agent: *
 Allow: /
-Disallow: /cdn-cgi
-
-User-agent: *
-Disallow: /
 
 Sitemap: ${new URL("sitemap-index.xml", site)}
 `;
